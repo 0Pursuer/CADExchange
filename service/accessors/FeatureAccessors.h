d8456def2f8f3d5138640f3cceebcff5a26c3f37
0Pursuer <3421248154@qq.com>
2025-12-16 19:33:00 +0800

    添加了一个头文件

---
+++ service/accessors/FeatureAccessors.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+// clang-format off
+#include "FeatureAccessorBase.h"
+#include "ReferenceAccessor.h"
+#include "SketchAccessor.h"
+#include "ExtrudeAccessor.h"
+#include "RevolveAccessor.h"
+#include "ModelAccessor.h"
+
+// clang-format on
+
+/**
+ * @brief 汇总所有 Accessor 接口，便于用户统一包含。
+ */
